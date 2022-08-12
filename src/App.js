d9c7f76f6c9c111709b87b4fd4cec38ac3c8337d
Frycen <ivanmatias1331@gmail.com>
2022-08-12 15:36:35 -0700

    added a side navigation bar for easy access to general links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navigation from "./components/navigation/navigation"
 import Welcome from "./components/welcome";
 import About from "./components/about";
+import SideInfo from "./components/sideInfo";
 import "./index.css"
 
 function App() {
   return (
     <div>
       <Navigation />
+      <SideInfo />
       <Welcome />
       <div className="spacer layer1" />
       <About />
---
+++ src/components/sideInfo.js
@@ -0,0 +1,52 @@
+import Icon from '@mdi/react'
+import { mdiGithub, mdiTwitter, mdiLinkedin, mdiEmailOutline } from '@mdi/js';
+import "../index.css"
+
+function SideInfo() {
+    const iconColor = "rgb(122,92,97)"
+
+    return(
+        <div className="sidebar-container">
+            <ul className="links-list-container">
+                <li>
+                    <a href="https://github.com/aishyuu" aria-label="GitHub" target="_blank" rel="noreferrer">
+                        <Icon path={mdiGithub}
+                            title="GitHub"
+                            size={1}
+                            color={iconColor}
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/BonnyBonBon_" aria-label="Twitter" target="_blank" rel="noreferrer">
+                        <Icon path={mdiTwitter}
+                            title="Twitter"
+                            size={1}
+                            color={iconColor}
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/ivan-matias-8171991ba/" aria-label="LinkedIn" target="_blank" rel="noreferrer">
+                        <Icon path={mdiLinkedin}
+                            title="Linkedin"
+                            size={1}
+                            color={iconColor}
+                        />
+                    </a>
+                </li>
+                <li className="final-link">
+                    <a href= "mailto: imatiascpp@gmail.com">
+                        <Icon path={mdiEmailOutline}
+                            title="Email"
+                            size={1}
+                            color={iconColor}
+                        />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default SideInfo
